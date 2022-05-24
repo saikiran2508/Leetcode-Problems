b2b73338cc25f903e5b25b77dc2df587b46c3964
Mohammed Imtiyaz <thisisimtiyaz01@gmail.com>
2022-05-24 14:07:57 +0530

    Time: 122 ms (20.30%), Space: 124 MB (15.63%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,30 +1,35 @@
 class Solution {
-    int dp[][][];
-    public int solve(int[] prices, int idx, int buy, int cap){
-        if(idx >= prices.length || cap <= 0) return 0;
-        if(dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
-        int m1 = 0;
-        if(buy == 1){
-            m1 = Math.max(solve(prices, idx+1, 1-buy, cap) - prices[idx],
-                    solve(prices, idx+1, buy, cap)
-                    );
-        }
-        else{
-            m1 = Math.max(solve(prices, idx+1, 1-buy, cap-1) + prices[idx],
-                    solve(prices, idx+1, buy, cap)
-                    );
-        }
-        return dp[idx][buy][cap] = m1;
-    }
     
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        dp = new int[n][2][3];
-        for(int[][] arr: dp){
-            for(int[] a: arr){
-                Arrays.fill(a,-1);
+        int dp[][][] = new int[n+1][2][3];
+        
+//         for(int idx = 0; i <= n; i++){
+//             for(int b = 0; b < 2; b++){
+//                 dp[idx][b][0] = 0;
+//             }
+//         }
+        
+//         for(int b = 0; b < 2; b++){
+//             for(int c = 0; c < 3; c++){
+//                 dp[n][b][c] = 0;
+//             }
+//         }
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int b = 0; b < 2; b++){
+                for(int c = 1; c < 3; c++){
+                    if(b == 1){
+                        dp[i][b][c] = Math.max(dp[i+1][1-b][c] - prices[i], dp[i+1][b][c]);
+                    }
+                    else{
+                        dp[i][b][c] = Math.max(dp[i+1][1-b][c-1]+prices[i], dp[i+1][b][c]);
+                    }
+                }
             }
         }
-        return solve(prices, 0, 1, 2);
+        return dp[0][1][2];
+        
+        
     }
 }
